677d3ba725e966be0b03b34988608e1e5477ea9d
Ryan Cline <ryancline88@yahoo.com>
2018-07-06 21:35:45 -0600

    first attempt

--- BalancedTernaryComputer/Program.cs
+++ BalancedTernaryComputer/Program.cs
@@ -6,7 +6,66 @@ namespace BalancedTernaryComputer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            int DEC = int.Parse(Console.ReadLine());
+
+            int largestExponent = (int)(Math.Ceiling(Math.Log(Math.Abs(DEC), 3)));
+            int arrSize = largestExponent + 1;
+
+            char[] chars = new char[arrSize];
+            if (DEC < 0)
+            {
+                chars[0] = 'T';
+                int currentValue = (int)(-1 * Math.Pow(3, largestExponent));
+                for (int i = 1, exp = largestExponent-1; i < chars.Length; i++, exp--)
+                {
+                    if (currentValue == DEC)
+                    {
+                        chars[i] = '0';
+                        continue;
+                    } else
+                    {
+                        int nextValue = (int)Math.Pow(3, exp);
+                        if (currentValue + nextValue <= DEC)
+                        {
+                            chars[i] = '1';
+                            currentValue += nextValue;
+                        } else
+                        {
+                            chars[i] = '0';
+                        }
+                    }
+                }
+            }
+            else
+            {
+                chars[0] = '1';
+                int currentValue = (int)(Math.Pow(3, largestExponent));
+                for (int i = 1, exp = largestExponent - 1; i < chars.Length; i++, exp--)
+                {
+                    if (currentValue == DEC)
+                    {
+                        chars[i] = '0';
+                        continue;
+                    }
+                    else
+                    {
+                        int nextValue = (int)Math.Pow(3, exp);
+                        if (currentValue - nextValue >= DEC)
+                        {
+                            chars[i] = 'T';
+                            currentValue -= nextValue;
+                        }
+                        else
+                        {
+                            chars[i] = '0';
+                        }
+                    }
+                }
+            }
+        
+
+            string result = string.Join("", chars);
+            Console.WriteLine(result);
         }
     }
 }
